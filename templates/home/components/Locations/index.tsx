b677273eabb9942d9bdd6e1753a365cc60db6941
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2026-06-12 15:47:50 -0300

    style: adjust grid

--- templates/home/components/Locations/index.tsx
+++ templates/home/components/Locations/index.tsx
@@ -34,7 +34,7 @@ export function Locations() {
       )}
 
       {hasLocations && (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-7 gap-4">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-7 gap-8 md:gap-4">
           {locations?.slice(0, 7).map((location) => (
             <LocationsCard key={location.id} location={location} />
           ))}
